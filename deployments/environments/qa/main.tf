44948e39bf943ea77405d1930fd1bfc21b07e3f1
yosefaziz <yosef.a@hotmail.de>
2024-01-26 17:44:08 +0000

    feat(infra): implement lambda infrastructure

---
+++ deployments/environments/qa/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "tf-backend"
+    key    = "qa/terraform.tfstate"
+    region = var.aws_region
+  }
+}
---
+++ deployments/modules/aws/lambda/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "role_arn" {
+  type = string
+}
+
+variable "policy_id" {
+  type = string
+}
