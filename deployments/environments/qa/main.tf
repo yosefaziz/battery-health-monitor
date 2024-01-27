2e5a6a216b7c54b3a16849d401ad2509543341c4
yosefaziz <yosef.a@hotmail.de>
2024-01-27 02:47:38 +0000

    chore(infra): add modules to qa

--- deployments/environments/qa/main.tf
+++ deployments/environments/qa/main.tf
@@ -14,3 +14,13 @@ terraform {
     region = var.aws_region
   }
 }
+
+module "aws_iam" {
+  source = "./../../modules/aws/iam"
+}
+
+module "aws_lambda" {
+  source    = "./../../modules/aws/lambda"
+  role_arn  = module.aws_iam.lambda_role_arn
+  policy_id = module.aws_iam.lambda_iam_policy_id
+}
